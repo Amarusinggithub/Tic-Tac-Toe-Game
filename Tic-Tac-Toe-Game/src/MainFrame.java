0076bcbf3db8f406c030388d11252e91fda07244
Amarusinggithub <loveamar285@gmail.com>
2023-10-20 10:52:08 -0500

    not finish

--- Tic-Tac-Toe-Game/src/MainFrame.java
+++ Tic-Tac-Toe-Game/src/MainFrame.java
@@ -66,7 +66,7 @@ public class MainFrame extends JFrame {
                 mainPanel.revalidate();
                 mainPanel.repaint();
             }
-            
+
         });
 
         newGameBtn.addActionListener(new ActionListener() {
@@ -87,6 +87,7 @@ public class MainFrame extends JFrame {
         setMinimumSize(new Dimension(300, 400));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+        
     }
     }
     public static void main(String[] args) throws Exception {
